57b4fd71a8d1fde9d7f4ea9ea7f32ce6217badfe
shaimalq <146936338+shaimalq@users.noreply.github.com>
2023-10-29 23:20:41 +0100

    Add files via upload

---
+++ function_max_min_reverse.py
@@ -0,0 +1,14 @@
+def maximum(t):
+  return max (t)
+def minimum(t1):
+  return min (t1)
+def inverse(t2):
+    t2.reverse()
+    return t2
+tab=[]
+for i in tab:
+   print("tab=[",i,"]=",end="")
+   tab.append(int-input())
+print("le maximum des elements:",maximum(tab))
+print("le maximum des elements:",minimum(tab))
+print("le maximum des elements:",inverse(tab))
